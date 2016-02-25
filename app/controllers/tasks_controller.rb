b2ce18bb3247410e426fd2a82ad40271b1d0aff6
Eugene Kunitsa <kunitsa.evgeniy@gmail.com>
2016-02-25 23:00:48 +0300

    DI-8: CRUD for tasks
    * Create Task CRUD

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,57 @@
+class TasksController < ApplicationController
+  layout 'task'
+
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+    @task = Task.new
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = 'Success'
+    else
+      flash[:danger] = 'Failed'
+    end
+    redirect_to tasks_path
+  end
+
+  def update
+    if @task.update(task_params)
+      flash[:success] = 'Success'
+    else
+      flash[:danger] = 'Failed'
+    end
+    redirect_to tasks_path
+  end
+
+  def destroy
+    if @task.destroy
+      flash[:success] = 'Success'
+    else
+      flash[:danger] = 'Failed'
+    end
+    redirect_to tasks_path
+  end
+
+  private
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:title, :description)
+    end
+end
